f792a0fd938b7855cafc7c6684e8fc765e00e281
Trish Ogen <patriciaogen@gmail.com>
2019-05-14 13:03:00 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if !phrase == phrase.upcase
+  if not phrase == phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
